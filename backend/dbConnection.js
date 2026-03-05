974f143593f2b9fc6b6f3871ae1c23318dfff55c
Sebas <sebgonrod@gmail.com>
2026-03-05 18:59:57 -0500

    Update db connection fix catch

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 
 mongoose.connect("mongodb+srv://sebas:beReo7aE5XipV4BU@cluster0.s3j7x8r.mongodb.net/?appName=Cluster0")
 .then(()=>{console.log("Conectado")})
-.catch((err)=>{console.log(err);})
+.catch((err)=>{console.log({"Ha sucedido el siguiente error" : err.message});})
 
