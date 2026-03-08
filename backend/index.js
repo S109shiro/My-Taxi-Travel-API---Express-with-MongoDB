dad4805040d96ea01b9fe3aa9d30775c85472582
Sebas <sebgonrod@gmail.com>
2026-03-08 18:30:24 -0500

    add CORS

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 // Linea para que me deje conectarme a Atlas
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
+// Importacion del modulo cors para poder consultar en otro servidor sin problemas de seguridad
+const cors = require("cors");
+
 // Importacion del modulo de Express
 const express = require("express");
 
 // Conexion a la base de datos desde un archivo externo
 require("./dbConnection");
+
 
 // Obtencion de los endpoints creados en otro archivo
 const usersRoutes = require("./routes/routesUser");
@@ -21,6 +25,8 @@ const travelRoutes = require("./routes/routesTravel");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 // Parsear datos a json
 app.use(express.json());
 
@@ -33,6 +39,8 @@ app.use("/reports", reportsRoutes);
 app.use("/gpsRecords", gpsRecordsRoutes);
 app.use("/qualifications", qualificationRoutes);
 app.use("/travels", travelRoutes);
+
+
 
 // Levantamiento del servidor
 app.listen(port, ()=>{
