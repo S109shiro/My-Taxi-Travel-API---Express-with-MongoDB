79db51aeb9cc2f0031679a5f988a84029e543788
Sebas <sebgonrod@gmail.com>
2026-03-08 10:51:02 -0500

    add Angular, create model and service usuario

---
+++ frontend/src/app/models/usuario.ts
@@ -0,0 +1,15 @@
+export class Usuario {
+    _id?: string;
+    nombre: string = "";
+    primer_apellido: string = "";
+    segundo_apellido: string = "";
+    edad: number = 0;
+    numero_identificacion: number = 0;
+    email: string = "@gmail.com";
+    sexo: string = "";
+    documento_identidad: string = "";
+    numero_telefono: string = "";
+    fecha_nacimiento: string = "1900-01-01";
+    calificacion_media: number = 0;
+    contrasena: string = "";
+}
---
+++ frontend/src/app/services/usuario.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Usuario } from '../models/usuario';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  // Colocar url de la api para usuarios
+  private urlApi = "http://localhost:3000/users";
+
+  constructor(private http: HttpClient){}
+
+  // Obtener todos los usuarios
+  getUsuarios(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(this.urlApi+"/getAll");  // Corregir endpoints
+  }
+
+  // Obtener un usuario
+  getUsuario(id: string): Observable<Usuario>{
+    return this.http.get<Usuario>(`${this.urlApi+"get"}/${id}`);
+  }
+
+  // Crear un usuario
+  postUsuario(newUsuario: Usuario): Observable<String>{
+    return this.http.post<String>(this.urlApi+"/create", newUsuario);
+  }
+
+  // Actualizar un usuario
+  putUsuario(id: string, updateUsuario: Usuario): Observable<String>{
+    return this.http.put<String>(`${this.urlApi+"update"}/${id}`, updateUsuario);
+  }
+
+  // Eliminar un usuario
+  deleteUsuario(id:string): Observable<String>{
+    return this.http.delete<String>(`${this.urlApi+"delete"}/${id}`);
+  }
+
+}
